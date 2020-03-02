b9b38f4b3e25a1cd1f907af9972a1ecfab0d7019
Marina <marina.g.ivanova@gmail.com>
2020-03-02 20:05:07 +0200

    app component/module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { TruckModule } from './truck/truck.module';
+import { SharedModule } from './shared/shared.module';
+import { ModalModule } from './modal/modal.module';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ModalTruckNewComponent } from './modal/modal-truck-new/modal-truck-new.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		NavbarComponent
 	],
 	imports: [
 		BrowserModule,
-		NgbModule
+		FormsModule,
+		ReactiveFormsModule,
+		NgbModule,
+		TruckModule,
+		ModalModule,
+		SharedModule,
 	],
-	providers: [],
+	entryComponents: [ModalTruckNewComponent],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
